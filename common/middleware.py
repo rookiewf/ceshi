e6468a6e2c21b0978e57f991d669a309f7e66aec
rookiewf <rookiewf@aliyun.com>
2020-04-20 13:23:21 +0800

    yyyyyyy

--- common/middleware.py
+++ common/middleware.py
@@ -8,3 +8,5 @@ class middlewareMix(MiddlewareMixin):
     def process_request(self,request):
         if request.path in self.request_list:
             print('this is process_request')
+        else:
+            print('this is not import request')
